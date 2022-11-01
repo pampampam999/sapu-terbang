de52b809dfb8b3f4fcdf8e030426d44d1c04cfa6
Billit Shotta <billitshotta@gmail.com>
2022-11-01 17:58:15 +0700

    adding query runner function and register gc to databases

--- run.py
+++ run.py
@@ -1,9 +1,31 @@
+from email import message_from_binary_file
 import telebot
 from telebot import types
 import datetime
 import logging
 import os
 from dotenv import load_dotenv,find_dotenv
+import sqlite3
+
+#sqlite
+#db_name = 'database.db'
+def run_query(query):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute(query)
+    conn.commit()
+    conn.close()
+
+'''
+cur.excute()
+from row in data:
+    print(row)
+
+#coomit buat manipulasi data
+conn.commit()
+conn.close()
+'''
+
 
 #Load .env variables
 load_dotenv(find_dotenv())
@@ -65,38 +87,55 @@ def send_role(message):
     bot.reply_to(message,'Pesan di kirim ke {} group terdaftar.'.format(totalgc))
 
     #gc pam
-    bot.send_message(chat_id=-1001734121931,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host))
+    bot.send_message(chat_id=-1001734121931,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host),parse_mode='MarkdownV2')
     #gc au indo gc
-    bot.send_message(chat_id=-1001746697467,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host))
+    bot.send_message(chat_id=-1001746697467,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host),parse_mode='MarkdownV2')
     #gc cucing
-    bot.send_message(chat_id=-1001765155506,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host))
+    bot.send_message(chat_id=-1001765155506,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host),parse_mode='MarkdownV2')
     #gc idn -1001607301547
-    bot.send_message(chat_id=-1001607301547,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host))
+    bot.send_message(chat_id=-1001607301547,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host),parse_mode='MarkdownV2')
 
 @bot.message_handler(commands=['roletest'])
 def send_roletest(message):
     log(message,'roletest')
+    
     texts = message.text.split(' ')
+        
     code = str(texts[1])
     mode = str(texts[2])
     host = str(texts[3])
-    
-    
+        
     bot.send_message(
         chat_id=-1001734121931, 
-        text="*bold* _italic_ `fixed width font` [link](http://google.com).", 
-        parse_mode='Markdown'
+        text="*bold \*text*", 
+        parse_mode='MarkdownV2'
         )  
-    
-    
-    
-    bot.send_message(chat_id=-1001734121931,text='`{}`\n\nMode : {}\nHost : {}'.format(code,mode,host),parse_mode='Markdown')
+            
+    bot.send_message(chat_id=-1001734121931,text='**`{code}` `{code}`\n`{code}` `{code}`**\n☝Click code to copy☝\n\nMode : {mode}\nHost : {host}'.format(code=code,mode=mode,host=host),parse_mode='MarkdownV2')
 
 @bot.message_handler(commands=['register'])
 def register(message):
     log(message,'register')
+    #mengambil data char
+    chat_id=message.chat.id #id group
     
-    bot.reply_to(message,'Group ID : ')
+    
+    #query database
+    query = 'INSERT INTO "group" (id_gc,nama_gc) values({chat_id},{chat_id});'.format(chat_id=chat_id)
+    run_query(query)
+    
+    #send massage if succes
+    bot.send_message(chat_id,'Group ID : {} Di Daftarkan'.format(chat_id))
+    
+    #conn = sqlite3.connect('database.db')
+    #cur = conn.cursor()
+    #cur.execute('''
+    #    INSERT INTO "group" (id_gc,nama_gc) values({chat_id},{chat_id});
+    #'''.format(chat_id=chat_id))
+    #conn.commit()
+    #conn.close()
+    
+    
 
 @bot.message_handler(commands=['groupinfo'])
 def info(message):
