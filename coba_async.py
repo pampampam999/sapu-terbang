d760524e055a14618c05c34b234b800e6ef79e9d
Billit Shotta <billitshotta@gmail.com>
2022-11-19 03:37:29 +0700

    add messsage handler /start and /help , add all text handdler

--- coba_async.py
+++ coba_async.py
@@ -1,8 +1,24 @@
 import asyncio
+from telebot.async_telebot import AsyncTeleBot
+import os
+import aiohttp
 
-async def main():
-    print('hello')
-    await asyncio.sleep(1)
-    print('world')
+#bot identity
+TOKEN = os.getenv('TOKEN')
+bot = AsyncTeleBot(TOKEN)
 
-asyncio.run(main())
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    await bot.send_message(-1001826122574, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.send_message(-1001826122574, message.text)
+
+
+asyncio.run(bot.polling())
